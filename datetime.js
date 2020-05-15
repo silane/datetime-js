05af36bbf95bd93618fb36c81ae3e3a8e010da9b
silane <18604696+silane@users.noreply.github.com>
2020-05-15 21:21:45 +0900

    Move difinitions of DateTimeErrors to "errors.js"

--- datetime.js
+++ datetime.js
@@ -1,3 +1,13 @@
+import {
+    DateTimeError, NotImplementedDateTimeError, TypeDateTimeError,
+    ValueDateTimeError, RangeDateTimeError,
+} from './errors.js';
+export {
+    DateTimeError, NotImplementedDateTimeError, TypeDateTimeError,
+    ValueDateTimeError, RangeDateTimeError,
+};
+
+
 /**
  * The smallest year number allowed in a date or datetime object.
  */
@@ -8,62 +18,8 @@ export const MINYEAR = 1
 export const MAXYEAR = 9999
 
 
-/**
- * The base class of the other exceptions in this module.
- * It is a subclass of Error.
- */
-export class DateTimeError extends Error {
-    /**
-     * @param {string} message Some error message.
-     */
-    constructor(message) {
-        super(message);
-        Object.defineProperty(this, 'name', {
-            configurable: true,
-            enumerable: false,
-            value: this.constructor.name,
-            writable: true,
-        });
-        if(Error.captureStackTrace) {
-            Error.captureStackTrace(this, this.constructor);
-        }
-    }
-}
-/**
- * Raised when derived classes should override the method.
- */
-export class NotImplementedDateTimeError extends DateTimeError {
-    constructor() {
-        super("Not implemented.");
-    }
-}
-export class TypeDateTimeError extends DateTimeError {
-    constructor(parameterName, parameterValue, message) {
-        super(message);
-        this.parameterName = parameterName;
-        this.parameterValue = parameterValue;
-    }
-}
-/**
- * Raised when an function receives an argument that has the right type but an
- * inappropriate value.
- */
-export class ValueDateTimeError extends DateTimeError {
-    /**
-     * @param {string} parameterName Parameter name of the function
-     * @param {*} parameterValue Passed value of the parameter
-     * @param {string} message Message
-     */
-    constructor(parameterName, parameterValue, message) {
-        super(message);
-        this.parameterName = parameterName;
-        this.parameterValue = parameterValue;
-    }
-}
-export class RangeDateTimeError extends ValueDateTimeError {
-}
-
 const stdDate = Function('return this')().Date;
+
 
 /**
  * "stdDate.UTC" converts years between 0 and 99 to a year in the 20th century.
---
+++ errors.js
@@ -0,0 +1,54 @@
+/**
+ * The base class of the other exceptions in this module.
+ * It is a subclass of Error.
+ */
+export class DateTimeError extends Error {
+    /**
+     * @param {string} message Some error message.
+     */
+    constructor(message) {
+        super(message);
+        Object.defineProperty(this, 'name', {
+            configurable: true,
+            enumerable: false,
+            value: this.constructor.name,
+            writable: true,
+        });
+        if(Error.captureStackTrace) {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+/**
+ * Raised when derived classes should override the method.
+ */
+export class NotImplementedDateTimeError extends DateTimeError {
+    constructor() {
+        super("Not implemented.");
+    }
+}
+export class TypeDateTimeError extends DateTimeError {
+    constructor(parameterName, parameterValue, message) {
+        super(message);
+        this.parameterName = parameterName;
+        this.parameterValue = parameterValue;
+    }
+}
+/**
+ * Raised when an function receives an argument that has the right type but an
+ * inappropriate value.
+ */
+export class ValueDateTimeError extends DateTimeError {
+    /**
+     * @param {string} parameterName Parameter name of the function
+     * @param {*} parameterValue Passed value of the parameter
+     * @param {string} message Message
+     */
+    constructor(parameterName, parameterValue, message) {
+        super(message);
+        this.parameterName = parameterName;
+        this.parameterValue = parameterValue;
+    }
+}
+export class RangeDateTimeError extends ValueDateTimeError {
+}
