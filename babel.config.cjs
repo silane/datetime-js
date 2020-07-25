94455cbc77f25732f0a0e24c612386f79e6d3db2
silane <18604696+silane@users.noreply.github.com>
2020-07-25 12:14:06 +0900

    Use only plugin-transform-modules-commonjs instead of preset-env to build cjs modules

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,20 +1,10 @@
 module.exports = {
   env: {
     'node-cjs': {
-      presets: [
-        [
-          '@babel/env',
-          {
-            targets: 'maintained node versions',
-            modules: 'cjs',
-            useBuiltIns: 'usage',
-            corejs: 3,
-          },
-        ],
-      ],
       plugins: [
-        ['replace-import-extension', { extMapping: { '.js': '.cjs' } }]
-      ]
+        ['replace-import-extension', { extMapping: { '.js': '.cjs' } }],
+         '@babel/transform-modules-commonjs',
+      ],
     },
   },
 };
